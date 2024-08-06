79f7ee6c125971289d614864c140641406b4908e
pedro.jesus <57639797+rufatop-jesus@users.noreply.github.com>
2024-08-06 17:25:01 -0300

    Add files via upload

--- initPositionsRSA.m
+++ initPositionsRSA.m
@@ -1,4 +1,4 @@
-        function [cell2parts, part2cells, PBIndex, X, Y, Z] = initPositionsRSA(nPart, partNumberDistribution, diameter, L)
+function [cell2parts, part2cells, nCells, cellSize, PBIndex, X, Y, Z] = initPositionsRSA(nPart, partNumberDistribution, diameter, L)
     
     % Initialize arrays
     PBIndex = zeros(nPart, 1);
@@ -7,14 +7,16 @@
     Z = zeros(nPart, 1);
     
     % PND weighted average diameter
-    avgDiameter = dot(partNumberDistribution,diameter) / nPart;
-    % avgDiameter = max(diameter,[],"all");
+    % avgDiameter = dot(partNumberDistribution,diameter) / nPart;
+    avgDiameter = max(diameter,[],"all");
 
     % Mesh of cubic cells to speed-up collision detection
     nCells = ceil(L * sqrt(3) / avgDiameter);
     cellSize = L / nCells;
-    cell2parts = cell(nCells,nCells,nCells); % list of particles within a cell   
-    part2cells = cell(nPart,1); % list of cells a particles occupies
+    cell2parts = zeros(power(nCells,3), ceil(nPart/power(nCells,3))); % list of particles within a cell. nPart/power(nCells,3) is the estimated max number of particles within a cell
+    nAddedParts = zeros(size(cell2parts,1),1); % array with the number of added particles on each cell of the cell2parts array.
+
+    part2cells = zeros(nPart, ceil(power(max(diameter,[],"all"),3)/ power(cellSize,3))); % list of cells a particles occupies. power(max(diameter,[],"all"),3)/ power(cellSize,3) is the estmated max number of cells a particle can occupy
 
     totAdParticles = 0; % total of adsorbed particles
 
@@ -22,7 +24,6 @@
     for i = size(partNumberDistribution, 1) : -1 : 1
         i
         adParticles = 0; % total of adsorbed particles of the current size
-        collision = 0;
 
         % While added particles < total of particles in the current size do
         while adParticles < partNumberDistribution(i,1)
@@ -35,12 +36,6 @@
             yCellTest = randi(nCells);
             zCellTest = randi(nCells);
 
-            % while numel(cell2parts{xCellTest, yCellTest, zCellTest}) > 0
-            %     xCellTest = randi(nCells);
-            %     yCellTest = randi(nCells);
-            %     zCellTest = randi(nCells);
-            % end
-
             % Generate a random position within the domain for the particle under adsorption
             xTest = (xCellTest - rand) * cellSize;
             yTest = (yCellTest - rand) * cellSize;
@@ -52,34 +47,39 @@
             Z(part) = zTest;
             
             % Identify the cells occupied by the particle under adsorption
-            cellsSub = initPart2Cells(nCells, cellSize, diameter(i,1), xTest, yTest, zTest);
-            cellsInd = sub2ind(size(cell2parts), cellsSub(:,1), cellsSub(:,2), cellsSub(:,3));
+            cells = initPart2Cells(nCells, cellSize, diameter(i,1), xTest, yTest, zTest);
+            cells = sub2ind([nCells,nCells,nCells], cells(:,1), cells(:,2), cells(:,3));
             
-            % For every cell in cells find the particles that occupy it.
-            collisionCandidates = cell2parts{cellsInd};
-            % collisionCandidates = unique(collisionCandidates);
+            % collisionCandidates = [];
+            % % For every cell in cells find the particles that occupy it.
+            % for testingCell = 1 : size(cellsInd,1)
+            %     if nAddedParts(cellsInd(testingCell)) > 0
+            %         collisionCandidates = [collisionCandidates cell2parts(cellsInd(testingCell), 1:nAddedParts(cellsInd(testingCell)))];
+            %     end
+            % end
+            collisionCandidates = unique(nonzeros(cell2parts(cells,:)));
 
-            isCollision = particleOverlapSphericalContPSD(L, diameter(PBIndex(part),1), X(part), Y(part), Z(part),...
-                                                          diameter(PBIndex(collisionCandidates),1), X(collisionCandidates), Y(collisionCandidates), Z(collisionCandidates));
+            % Test for collision
+            collision = particleOverlapSphericalContPSDBool(L, diameter(PBIndex(part),1), X(part), Y(part), Z(part),...
+                                                            diameter(PBIndex(collisionCandidates),1),...
+                                                            X(collisionCandidates), Y(collisionCandidates), Z(collisionCandidates));
             
             % If there is no collision do
-            if not(isCollision)
-                
-                % Register the cells the particle under adsorption occupy in the part2cells array
-                part2cells{part,1} = cellsSub; 
+            if not(collision)
 
-                % % Register the particle under adsorption in the cell2parts array 
-                for adsorbingCell = 1 : size(cellsSub,1)
-                    cell2parts{cellsSub(adsorbingCell,1), cellsSub(adsorbingCell,2), cellsSub(adsorbingCell,3)} = [cell2parts{cellsSub(adsorbingCell,1), cellsSub(adsorbingCell,2), cellsSub(adsorbingCell,3)} part];
+                % Register the cells the particle under adsorption occupy in the part2cells array
+                part2cells(part, 1 : size(cells,1)) = cells'; 
+
+                % Register the particle under adsorption in the cell2parts array 
+                for adsorbingCell = 1 : size(cells,1)
+                    cell2parts(cells(adsorbingCell,1), nAddedParts(adsorbingCell)+1) = part;
                 end
 
-                adParticles = adParticles + 1; % sum one in the adsorbed particles counter                  
-            else
-                collision = collision + 1;
+                adParticles = adParticles + 1; % sum one in the adsorbed particles counter       
+                nAddedParts(cells,1) = nAddedParts(cells,1) + 1; % sum one on the cells the particle 'part' occupies
             end
          
         end
-        collision / adParticles
         totAdParticles = totAdParticles + adParticles; % sum the counter of the size to the aggregate counter
     end
 end
